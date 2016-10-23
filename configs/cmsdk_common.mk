8a28ec29c143b928af705d2ceca14038d691dbce
Chris <chriscrawf95@gmail.com>
2016-10-22 18:59:57 -0700

    cmsdk_common: Temporarily add back cmsdk api

--- configs/cmsdk_common.mk
+++ configs/cmsdk_common.mk
@@ -22,3 +22,28 @@ PRODUCT_PACKAGES += \
 # JNI Libraries
 PRODUCT_PACKAGES += \
     libcmsdk_platform_jni
+	
+ifndef CM_PLATFORM_SDK_VERSION
+  # This is the canonical definition of the SDK version, which defines
+  # the set of APIs and functionality available in the platform.  It
+  # is a single integer that increases monotonically as updates to
+  # the SDK are released.  It should only be incremented when the APIs for
+  # the new release are frozen (so that developers don't write apps against
+  # intermediate builds).
+  CM_PLATFORM_SDK_VERSION := 7
+endif
+
+ifndef CM_PLATFORM_REV
+  # For internal SDK revisions that are hotfixed/patched
+  # Reset after each CM_PLATFORM_SDK_VERSION release
+  # If you are doing a release and this is NOT 0, you are almost certainly doing it wrong
+  CM_PLATFORM_REV := 0
+endif
+
+# CyanogenMod Platform SDK Version
+PRODUCT_PROPERTY_OVERRIDES += \
+  ro.aoscp.build.version.plat.sdk=$(CM_PLATFORM_SDK_VERSION)
+
+# CyanogenMod Platform Internal
+PRODUCT_PROPERTY_OVERRIDES += \
+  ro.aoscp.build.version.plat.rev=$(CM_PLATFORM_REV)

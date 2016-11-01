e382a56182cbc512007da87b1ae17eb9320e0b70
Aaron Nixon <aarondoom@cookiedoom.com>
2016-11-01 11:37:48 -0700

    Include the version in all builds (whoops)
    Change-Id: I33661491cd35e98385cc64d91824ca064e84eeef

--- configs/common.mk
+++ configs/common.mk
@@ -32,10 +32,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 AOSCP_ZIP_VERSION := $(AOSCP_VERSION)
 
 ifeq ($(AOSCP_BUILDTYPE),unofficial)
-	AOSCP_ZIP_VERSION := $(shell date -u +%Y%m%d)
+	AOSCP_ZIP_VERSION := $(AOSCP_VERSION)-$(shell date -u +%Y%m%d)
 else
 ifeq ($(AOSCP_BUILDTYPE),nightly)
-	AOSCP_ZIP_VERSION := $(shell date -u +%Y%m%d)
+	AOSCP_ZIP_VERSION := $(AOSCP_VERSION)-$(shell date -u +%Y%m%d)
 endif # nightly
 endif
 

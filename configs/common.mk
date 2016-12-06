879456bf278d496dd2c39b11b32c71a8bb1900ed
Chris <chriscrawf95@gmail.com>
2016-12-05 23:01:17 -0500

    CypherOS 3.1.4 Release 13
    Change-Id: Id32c7ebbac1c30d52a8ae17c8e0ac717f81ebd66

--- configs/common.mk
+++ configs/common.mk
@@ -4,9 +4,9 @@ SUPERUSER_EMBEDDED := true
 
 # Include versioning information
 # Format: Major.minor.maintenance(-TAG)
-export AOSCP_VERSION := 3.1.3
+export AOSCP_VERSION := 3.1.4
 
-export AOSCP_RELEASE_ID := CNRC1012
+export AOSCP_RELEASE_ID := CNRC1013
 
 export AOSCP_API_LEVEL := Cheesecake
 
